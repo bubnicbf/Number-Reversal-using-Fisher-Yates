146e69ecfe1d28b794fce90c9d3a1231c87dcd52
Ben Bubnick <ben.bubnick@gmail.com>
2015-04-05 09:34:49 -0400

    Create Sattolo-algorithm.py

---
+++ Sattolo-algorithm.py
@@ -0,0 +1,9 @@
+from random import randrange
+ 
+def sattoloCycle(items):
+    i = len(items)
+    while i > 1:
+        i = i - 1
+        j = randrange(i)  # 0 <= j <= i-1
+        items[j], items[i] = items[i], items[j]
+    return
